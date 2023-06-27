81ac4a0ed1ef6849c971c017f462ad9b43a601f0
nolanrummel <132599289+nolanrummel@users.noreply.github.com>
2023-06-27 16:22:42 -0400

    Update MonsterCard.js

--- my-app/src/components/MonsterCard.js
+++ my-app/src/components/MonsterCard.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function MonsterCard() {
+function MonsterCard({id, name, tier, image, abilities, handleSelect}) {
+  const [selected, setSelected] = useState(false)
+
+  const handleClick = () => {
+    setSelected(!selected)
+    handleSelect(id)
+  }
+
+  // const selectedCard =    
+  // <div>
+  //   <h3>{name}</h3>
+  //   <ul>
+  //     <h5>Abilities</h5>
+  //     <div>{moveset}</div>
+  //   </ul>
+  //   <h5>Tier: {tier}</h5>
+  // </div>
+
+
   return (
-    <div>MonsterCard</div>
+    <div onClick={handleClick}>
+      {/* {selected ? selectedCard : <img src={image} alt={name}/>} */}
+      <img src={image} alt={name}/>
+    </div>
   )
 }
 
