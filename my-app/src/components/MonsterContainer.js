3e0c5c5e4f588bd5161fa30fd7bf114b47fb7490
nolanrummel <132599289+nolanrummel@users.noreply.github.com>
2023-06-27 16:23:01 -0400

    Update MonsterContainer.js

--- my-app/src/components/MonsterContainer.js
+++ my-app/src/components/MonsterContainer.js
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
+import MonsterCard from './MonsterCard'
 
-function MonsterContainer() {
+function MonsterContainer({cohort, teamName, fighterList, trophies}) {
+  const [displayedCoder, setDisplayedCoder] = useState(cohort[0])
+
+  const handleSelect = (id) => {
+    const selectedCoder = cohort.filter((singleCoder) => singleCoder.id === id)
+    setDisplayedCoder(selectedCoder[0])
+  }
+
+  const moveset = displayedCoder.abilities.map(moves => { 
+    return (
+      <ul>
+        <p>{moves.firstattack}</p>
+        <p>{moves.secondattack}</p>
+        <p>{moves.thirdattack}</p>
+      </ul>
+    )
+  })
+
+  const renderedCoders = cohort.map(coder => 
+    <MonsterCard
+      key={coder.id}
+      id={coder.id}
+      name={coder.name}
+      tier={coder.tier}
+      image={coder.image}
+      abilities={coder.abilities}
+      handleSelect={handleSelect}
+    />
+  )
+
   return (
-    <div>MonsterContainer</div>
+    <div>
+      <div>
+        <h1>Team Info</h1>
+          <h2>{teamName}</h2>
+          <ul>{fighterList}</ul>
+          <ul>{trophies}</ul>
+      </div>
+      <div>
+        <h1>Fighter Info</h1>
+          <div>
+            <h3>{displayedCoder.name}</h3>
+            <ul>
+              <h5>Abilities</h5>
+              {moveset}
+            </ul>
+            <h5>{displayedCoder.tier}</h5>
+          </div>
+      </div>
+      <div>
+        <h1>Choose Your Team</h1>
+        {renderedCoders}
+      </div>
+    </div>
+    
   )
 }
 
