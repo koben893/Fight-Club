f008b0a976dbdf28885dd872bea16f0c3871a120
Nolan Rummel <nolan.rummel22@gmail.com>
2023-06-27 10:57:31 -0400

    fetchs on app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Home from './components/Home'
 
 function App() {
+  const [cohort, setCohort] = useState([])
+  const [userList, setUserList] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/fighters")
+      .then(r => r.json())
+      .then(coder => setCohort(coder))
+  }, [])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then(r => r.json())
+      .then(user => setUserList(user))
+  }, [])
+  
   return (
     <div className="App">
       <Home />
