0f72a8fdfb542d1550baefa22d4779ad9e160d6f
Nolan Rummel <nolan.rummel22@gmail.com>
2023-06-27 16:20:04 -0400

    teampage, monstercard and monstercontainer first commit

--- my-app/src/components/BattleTeamPage.js
+++ my-app/src/components/BattleTeamPage.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import MonsterContainer from './MonsterContainer'
 
-function BattleTeamPage() {
+function BattleTeamPage({cohort, userList}) {
+  const teamInfo = userList.map(user => 
+    <MonsterContainer 
+      cohort={cohort}
+      key={user.id}
+      teamName={user.teamName}
+      fighterList={user.fighterList}
+      trophies={user.trophies}
+    />
+    )
   return (
-    <div>BattleTeamPage</div>
+    <div>
+      {teamInfo}
+    </div>
   )
 }
 
