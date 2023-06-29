59bdc25a9ece098fc645555ddfb85f4be7edbe3f
TJ Stifter <tjstifter@gmail.com>
2023-06-29 09:04:03 -0400

    comment

--- my-app/src/components/TeamContainer.js
+++ my-app/src/components/TeamContainer.js
@@ -10,10 +10,10 @@ function TeamContainer({teamList, handleSelect, handleTeamUpdate, activeUser}) {
           handleSelect={handleSelect}
         /> 
     ) 
-
+        const renderHeader = activeUser.name ? "Change Your Team Below" : "Choose Your Team Below"
     return (
         <div>
-            <h3>Team Members</h3>
+            <h3>{renderHeader}</h3>
             {renderedTeams}
             <button onClick={() => handleTeamUpdate(teamList)} disabled={!activeUser.name}>Confirm Changes to Your Team</button>
         </div>
