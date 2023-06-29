956eb8d8acdd054e8e225ddf73e445b1a812bd0d
Andrew OBrien <aobrienaf@gmail.com>
2023-06-29 15:39:21 -0500

    opponent mapping

--- my-app/src/components/OpponentGen.js
+++ my-app/src/components/OpponentGen.js
@@ -8,12 +8,14 @@ function OpponentGen({opponents, randomOpponents}) {
         return (randomOpponents[0] === opponents.id || randomOpponents[1] === opponents.id || randomOpponents[2] === opponents.id)
     }
         )
-        
+    const renderOpponentList = filterOpponents.map(coder=><li key={coder.id}>{coder.name}</li>)
 console.log(filterOpponents)
+
     return (
-    <div className='preview-card'>
-        {/* <h5>{filterOpponents}</h5> */}
-    </div>
+        <div className='preview-card'>
+            <h5>{name} {abilities}</h5>
+            <ul>{renderOpponentList}</ul>
+        </div>
     )
 }
 
