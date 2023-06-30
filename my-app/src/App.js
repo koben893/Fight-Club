c828b2b3287213cd74f5b7a4d37e79f0e5141af9
TJ Stifter <tjstifter@gmail.com>
2023-06-30 09:10:58 -0400

    swapped routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,21 +12,21 @@ function App() {
   const [userList, setUserList] = useState([])
   const [activeUser, setActiveUser] = useState({})
   const [opponentTeam, setOpponentTeam] = useState([]);
- 
+
   const handleLogInUser = (activeUser) => setActiveUser(activeUser)
 
   const handleUpdateActUser = (newUser) => {
     setActiveUser(newUser)
-    setUserList(current=>[...current, newUser])
+    setUserList(current => [...current, newUser])
   }
 
   const handleTeamUpdate = (teamList) => {
-    if (teamList.length < 3 || teamList.length > 3) alert('need 3 coders only')
+    if (teamList.length !== 3) alert('need 3 coders only')
     else if (activeUser.name) setActiveUser(current => ({ ...current, fighterList: teamList }))
     else alert('no one is signed in')
   }
 
-  const handleOpponentTeam = (team) =>setOpponentTeam(team);
+  const handleOpponentTeam = (team) => setOpponentTeam(team);
 
   useEffect(() => {
     fetch("http://localhost:3000/fighters")
@@ -46,19 +46,18 @@ function App() {
       <Login userList={userList} handleLogInUser={handleLogInUser} activeUser={activeUser} />
       <Navbar />
       <Switch>
-        <Route path="/teamPage">
-          <BattleTeamPage
-            cohort={cohort} activeUser={activeUser} handleUpdateActUser={handleUpdateActUser} handleTeamUpdate={handleTeamUpdate} />
+        <Route path="/pregame">
+          <Home cohort={cohort} activeUser={activeUser} handleOpponentTeam={handleOpponentTeam} opponentTeam={opponentTeam} />
         </Route>
         <Route path="/arena">
-          <BattleArenaPage activeUser={activeUser} opponentTeam={opponentTeam}/>
+          <BattleArenaPage activeUser={activeUser} opponentTeam={opponentTeam} />
         </Route>
         <Route path="/trophies">
           <TrophiesPage />
         </Route>
         <Route exact path="/">
-          <Home cohort={cohort} activeUser={activeUser} handleOpponentTeam={handleOpponentTeam} opponentTeam={opponentTeam}/>
- 
+          <BattleTeamPage
+            cohort={cohort} activeUser={activeUser} handleUpdateActUser={handleUpdateActUser} handleTeamUpdate={handleTeamUpdate} />
         </Route>
       </Switch>
     </div>
